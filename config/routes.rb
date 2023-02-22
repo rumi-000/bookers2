c8bb2613a03b0fdc8c5bb059cc12ddd6d0f20d40
rumi-000 <rumi.sato9797@gmail.com>
2023-02-22 12:24:27 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  get 'about', to: 'homes#about'
+  
   root to: 'homes#top'
+
   
-  
+  get '/home/about', to: 'homes#about'
   resources :books, only: [:new, :index, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update, :index,]
-  
 end
